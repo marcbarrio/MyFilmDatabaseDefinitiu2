d9644d290be44e04a23c891828e3bcb78fd89542
marcbarrio <marc.barrio95@gmail.com>
2017-01-11 07:24:29 +0100

    Main diferent ordres

--- app/src/main/java/com/example/pr_idi/mydatabaseexample/MainActivity.java
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/MainActivity.java
@@ -23,13 +23,13 @@ import android.widget.ListView;
 public class MainActivity extends AppBasicActivity {
 
     FilmData filmData;
-
+    String ordre = MySQLiteHelper.COLUMN_YEAR_RELEASE;
    @Override
     public void onResume(){
-        super.onResume();
-        filmData.open();
-        String ordreAny = MySQLiteHelper.COLUMN_YEAR_RELEASE;
-        List<Film> values = filmData.getAllFilmsOrder(ordreAny);
+       super.onResume();
+       filmData.open();
+
+        List<Film> values = filmData.getAllFilmsOrder(ordre);
         final ArrayAdapter<Film> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, values);
         ListView listView = (ListView) findViewById(R.id.list_main_activity);
@@ -47,13 +47,13 @@ public class MainActivity extends AppBasicActivity {
         filmData = new FilmData(this);
         filmData.open();
 
-        List<Film> values = filmData.getAllFilms();
-        Collections.sort(values,new Comparator<Film>(){
+        List<Film> values = filmData.getAllFilmsOrder(ordre);
+        /*Collections.sort(values,new Comparator<Film>(){
             @Override
             public int compare(Film f1, Film f2){
                 return f1.getTitle().compareTo(f2.getTitle());
             }
-        });
+        });*/
         final ArrayAdapter<Film> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, values);
         ListView listView = (ListView) findViewById(R.id.list_main_activity);
@@ -68,6 +68,38 @@ public class MainActivity extends AppBasicActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void onClick(View view) {
+        //@SuppressWarnings("unchecked")
+        //ArrayAdapter<Film> adapter = (ArrayAdapter<Film>) getListAdapter();
+        //Film film;
+        switch (view.getId()) {
+            case R.id.order_nota:
+                ordre = MySQLiteHelper.COLUMN_CRITICS_RATE;
+                Log.d("Ordre: ",ordre);
+                onResume();
+                //startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                break;
+            case R.id.order_title:
+                ordre = MySQLiteHelper.COLUMN_TITLE;
+                Log.d("Ordre: ",ordre);
+                onResume();
+                //startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                break;
+            case R.id.order_year:
+                ordre = MySQLiteHelper.COLUMN_YEAR_RELEASE;
+                Log.d("Ordre: ",ordre);
+                onResume();
+                //startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                break;
+        }
+        /*Log.d("Main","typeoforder " + String.valueOf(typeofOrder));
+        ordre = MySQLiteHelper.COLUMN_TITLE;
+        List<Film> values = filmData.getAllFilmsOrder(ordre);
+        for(int i = 0; i < values.size(); ++i){
+            Log.d("Film ordered"+"i ",values.get(i).getTitle());
+        }*/
     }
 
     // Will be called via the onClick attribute
